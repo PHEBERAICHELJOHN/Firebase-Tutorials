4009e10439ab13b7c65fd42109ef17e1bbc036e2
pheberaicheljohn <pheberaichaljohn@gmail.com>
2019-03-24 08:41:25 +0530

    added name and email

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/google/android/gms/base/R.java
@@ -68,9 +68,9 @@ public final class R {
         public static final int dark = 0x7f070035;
         public static final int icon_only = 0x7f07004c;
         public static final int light = 0x7f070053;
-        public static final int none = 0x7f07005d;
-        public static final int standard = 0x7f070086;
-        public static final int wide = 0x7f07009c;
+        public static final int none = 0x7f07005f;
+        public static final int standard = 0x7f070088;
+        public static final int wide = 0x7f07009e;
     }
     public static final class string {
         private string() {}
--- app/src/main/java/com/example/firebasetutorials/MainActivity.java
+++ app/src/main/java/com/example/firebasetutorials/MainActivity.java
@@ -4,9 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.google.android.gms.common.SignInButton;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     // instance / reference to database
 
     private DatabaseReference mDatabase;
+
+    private EditText mNameField;
+    private EditText mMailField;
 
 
     @Override
@@ -25,8 +32,16 @@ public class MainActivity extends AppCompatActivity {
         mfirebase_btn = (Button) findViewById(R.id.firebase_btn);
 
         //variable pointing to root directory
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        //here we don't want to create the child User_01 in db. it will automatically create it.
+        //mDatabase = FirebaseDatabase.getInstance().getReference().child("User_01");
+
+        //going to child link
+        // mDatabase = FirebaseDatabase.getInstance().getReference().child("registration");
+
+        mNameField = (EditText) findViewById(R.id.name_field);
+        mMailField = (EditText) findViewById(R.id.mail_field);
 
 
         mfirebase_btn.setOnClickListener(new View.OnClickListener() {
@@ -37,7 +52,31 @@ public class MainActivity extends AppCompatActivity {
                 // 1. Create child in its root
                 // 2. Assign some value to child
 
-           //     mDatabase.child("name").setValue("JESUS");
+
+             //   mDatabase.child("name").setValue("JESUS"); //directly add JESUS to db
+
+                String name = mNameField.getText().toString().trim();
+                String mail = mMailField.getText().toString().trim();
+
+              //  mDatabase.child("name").setValue(name);
+
+                //generating random keys for each insertion of user
+                //push() generates random key each time
+
+
+                //for inserting more than one value :
+                // create Hashmap
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name",name);
+                dataMap.put("Email",mail);
+
+                mDatabase.push().setValue(dataMap);
+
+
+
+               // mDatabase.push().setValue(name);
+
+
 
             }
         });
